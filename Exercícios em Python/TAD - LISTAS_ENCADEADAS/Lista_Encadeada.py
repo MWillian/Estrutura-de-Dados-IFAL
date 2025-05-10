73d4baeb8e515e8a29e0cb9f77b9397593b046d6
MWillian <matheuswno02@gmail.com>
2025-05-10 02:30:00 -0300

    finalização dos arquivos do mini projeto

--- Exercícios em Python/TAD - LISTAS_ENCADEADAS/Lista_Encadeada.py
+++ Exercícios em Python/TAD - LISTAS_ENCADEADAS/Lista_Encadeada.py
@@ -1,51 +1,54 @@
 from Noh import Noh
 
+
 class ListaNaoOrdenada:
     def __init__(self):
         self.head = None
-    
+
     def is_empty (self):
-        return self.head == None
+            return self.head == None
 
     def add_fim(self, item):
-        temp = Noh(item)
-        if self.head is None:
-            self.head = temp
-        else:
-            atual = self.head
-            while atual.getNext() is not None:
-                atual = atual.getNext()
-            atual.setNext(temp)
+            temp = Noh(item)
+            if self.head is None:
+                self.head = temp
+            else:
+                atual = self.head
+                while atual.getNext() is not None:
+                    atual = atual.getNext()
+                atual.setNext(temp)
 
     def size (self):
-        atual = self.head
-        contador = 0
-        while atual != None:
-            contador += 1
-            atual = atual.getNext()
-        return contador
+            atual = self.head
+            contador = 0
+            while atual != None:
+                contador += 1
+                atual = atual.getNext()
+            return contador
     
     def search (self,item):
-        atual = self.head
-        encontrou = False
-        while atual != None and not encontrou:
-            if atual.getData() == item:
-                encontrou = True
-            else:
-                atual = atual.getNext()
-        return encontrou
+            atual = self.head
+            encontrou = False
+            while atual != None and not encontrou:
+                if atual.getData() == item:
+                    encontrou = True
+                else:
+                    atual = atual.getNext()
+            return encontrou
     
     def remove_first(self):
-        if self.head is not None:
-            self.head = self.head.getNext()
-        else:
-            print("Não é possível remover Nós, a lista está vazia")
-        
+            if self.head is not None:
+                self.head = self.head.getNext()
+            else:
+                print("Não é possível remover Nós, a lista está vazia")
+            
     def print_lista(self):
-        atual = self.head
-        print("Estado da lista atual:")
-        if atual is None:
-            print("Vazia\n")
-        while atual is not None:
-            print(atual.getData())
-            atual = atual.getNext()
+            atual = self.head
+            print("Estado da lista atual:")
+            if atual is None:
+                print("Vazia\n")
+                print("--------------------------------------")
+            while atual is not None:
+                print(atual.getData())
+                atual = atual.getNext()
+            print("--------------------------------------")
--- Exercícios em Python/TAD - LISTAS_ENCADEADAS/Thread - cenário com a exclusão mutua.py
+++ Exercícios em Python/TAD - LISTAS_ENCADEADAS/Thread - cenário com a exclusão mutua.py
@@ -4,34 +4,44 @@ from Noh import *
 import time
 
 
+# Instância do bloqueio de acesso (Lock)
+lock = threading.Lock()
+
 # Variáveis simbólicas para exibir a ordem dos Nós adicionados e removidos
 contador_insercao = 1
 contador_remocao = 1
-
 
 # Função para inserir Nós no fim na lista. A quantidade de Nós é recebida como parâmetro na função. 
 # Para visualizar o estado da lista, os valores de cada Nó são retornados a cada inserção ou remoção.
 def inserirXVezes(lista:ListaNaoOrdenada,quant):
     global contador_insercao
-    for i in range(1,quant+1):
-        time.sleep(5)
-        print(f"Adicionando o {contador_insercao}º nó.")
-        lista.add_fim(i)
-        lista.print_lista()
-        print()
-    print("Inserção Finalizada")
+
+    # o lock aqui protege toda a área região crítica de ser acessadas por outras Threads.
+    with lock: 
+        for i in range(0,quant):
+            time.sleep(5)
+            print(f"Adicionando o {contador_insercao}º nó.")
+            lista.add_fim(i)
+            lista.print_lista()
+            print()
+            contador_insercao +=1
+        print("Inserção Finalizada")
 
 
 # Função para remover o Nó da lista. A remoção ocorre no começo da lista.
 def removerXVezes(lista:ListaNaoOrdenada,quant):
     global contador_remocao
-    for i in range(1,quant+1):
-        time.sleep(5)
-        print(f"Removendo o {contador_remocao}º nó.")
-        lista.remove_first()
-        lista.print_lista()
-        print()
-    print("Remoção Finalizada")
+
+    # o lock aqui protege toda a área região crítica de ser acessadas por outras Threads.
+    with lock:
+        for i in range(0,quant):
+            time.sleep(5)
+            print(f"Realizando a {contador_remocao}ª remoção.")
+            lista.remove_first()
+            lista.print_lista()
+            print()
+            contador_remocao +=1
+        print("Remoção Finalizada")
 
 
 # Instância da lista encadeada
@@ -45,15 +55,13 @@ Thread3 = threading.Thread(target=removerXVezes,args=(ListaExemplo, 60))
 Thread4 = threading.Thread(target=removerXVezes,args=(ListaExemplo, 60))
 
 
-# Inicialização das threads de forma simultânea
+# Inicializando as threads e finalizando as threads
 # O método .join impede que a mesma Thread instânciada seja chamada sem que esta mesma Thread seja finalizada.
 Thread1.start()
 Thread2.start()
 Thread3.start()
 Thread4.start()
 
-
-# Finalização das threads
 Thread1.join()
 Thread2.join()
 Thread3.join()
